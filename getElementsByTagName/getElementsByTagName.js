5040372493459211b1169bb225ebb8491b5605f3
Kiran R <kiran.r1497@gmail.com>
2025-08-01 14:14:27 +0530

    first commit

---
+++ getElementsByTagName/getElementsByTagName.js
@@ -0,0 +1,8 @@
+const listItems = document.getElementsByTagName('li');
+
+for (let i = 0; i < listItems.length; i++)
+{
+    listItems[i].style.fontStyle = "italic"; 
+}
+
+listItems[4].style.color = "blue"; 
